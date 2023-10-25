a22d2f9ae8d0106ce1e5fb1c57f6419d44f5015e
YunHak Lee <lyh990408@gmail.com>
2023-10-25 14:16:54 +0900

    [SETTING] 프로젝트 시작을 위한 세팅(#1) - codeBasedUI Setting, Font&Asset 추가

---
+++ SOPT_Assignment/SOPT_Assignment/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT_Assignment
+//
+//  Created by 이윤학 on 10/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
