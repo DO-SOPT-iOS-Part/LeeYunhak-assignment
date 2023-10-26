1f7a9a3a6563fb509105aaabe19249a850d998d4
YunHak Lee <lyh990408@gmail.com>
2023-10-26 22:17:52 +0900

    [ADD] 원하는 부분만 border를 넣을 수 있는 extension 추가(#1)-UIStackView extension 함수명 변경

---
+++ SOPT_Assignment/SOPT_Assignment/CustomView/WeatherStateView.swift
@@ -0,0 +1,74 @@
+//
+//  wetherTimeLineView.swift
+//  SOPT_Assignment
+//
+//  Created by 이윤학 on 10/26/23.
+//
+
+import UIKit
+import SnapKit
+
+final class WeatherStateView: UIView {
+    
+    var timeText: String? = "Now"
+    var weatherimageName: String = ""
+    var temperature: Int = 21
+    
+    lazy var weatherStateStackView = UIStackView()
+    lazy var timeLabel = UILabel()
+    lazy var weatherIcon = UIImageView()
+    lazy var temperatureLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupStyle()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupStyle()
+        setupLayout()
+    }
+    
+    func setupStyle() {
+        
+        weatherStateStackView.setupStyle {
+            $0.axis = .vertical
+            $0.alignment = .center
+            $0.spacing = 14
+        }
+        
+        timeLabel.setupStyle {
+            $0.setBasic(
+                font: UIFont(name: "SFProDisplay-Medium", size: 17),
+                text: timeText
+            )
+        }
+        
+        weatherIcon.setupStyle {
+            $0.image = UIImage(named: weatherimageName)
+        }
+        
+        temperatureLabel.setupStyle {
+            $0.setBasic(
+                font: UIFont(name: "SFProDisplay-Medium", size: 22),
+                text: "\(temperature)°"
+            )
+        }
+        
+    }
+    
+    func setupLayout() {
+        self.addSubViews(weatherStateStackView)
+        
+        weatherStateStackView.addArrangedSubviews(
+            timeLabel, weatherIcon, temperatureLabel
+        )
+        
+        weatherStateStackView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+        }
+
+    }
+}
